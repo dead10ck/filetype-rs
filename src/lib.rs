8a859170bc9cf4c5ca54853294b2f200beb077e9
Skyler Hawthorne <skyler@opendns.com>
2015-04-27 15:39:50 -0700

    Init

---
+++ src/lib.rs
@@ -0,0 +1,45 @@
+extern crate libc;
+extern crate nix;
+
+use std::fs::File;
+use std::os::unix::io::AsRawFd;
+use std::convert::From;
+use nix::sys::stat::fstat;
+use libc::consts::os::posix88;
+
+/// Every standard Unix file type.
+pub enum FileType {
+    Regular,
+    Directory,
+    Symlink,
+    NamedPipe,
+    Socket,
+    Device,
+}
+
+pub enum Error {
+    NixError(nix::Error),
+    UnknownFileType,
+}
+
+impl From<nix::Error> for Error {
+    fn from(nix_err : nix::Error) -> Self {
+        Error::NixError(nix_err)
+    }
+}
+
+/// Returns the file type of `f`.
+pub fn file_type(f : &File) -> Result<FileType, Error> {
+    let fd = f.as_raw_fd();
+    let fstat = try!(fstat(fd));
+    let file_mask = fstat.st_mode & posix88::S_IFMT;
+    get_file_type(&file_mask)
+}
+
+fn get_file_type(file_mask : &u16) -> Result<FileType, Error> {
+    match *file_mask {
+        posix88::S_IFREG => Ok(FileType::Regular),
+        posix88::S_IFDIR => Ok(FileType::Directory),
+        _ => Err(Error::UnknownFileType),
+    }
+}
