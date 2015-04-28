59ec049f4df1b1e678258f92a11afa566cb4df3a
Skyler Hawthorne <skylerhawthorne@gmail.com>
2015-04-28 11:46:46 -0700

    Test directory

--- src/test/mod.rs
+++ src/test/mod.rs
@@ -1,13 +1,21 @@
-use super::FileType;
-use super::UnixFileType;
-use std::fs;
-use std::fs::File;
-use std::result::Result;
+#[cfg(test)] use super::FileType;
+#[cfg(test)] use super::UnixFileType;
+#[cfg(test)] use std::fs;
+#[cfg(test)] use std::fs::File;
 
 #[test]
 fn regular_file() {
     let fname = "foo";
     let f = File::create(fname).unwrap();
     assert_eq!(f.file_type().unwrap(), FileType::Regular);
-    fs::remove_file(fname);
+    assert!(fs::remove_file(fname).is_ok());
+}
+
+#[test]
+fn diretory() {
+    let dirname = "testdir";
+    assert!(fs::create_dir(dirname).is_ok());
+    let f = File::open(dirname).unwrap();
+    assert_eq!(f.file_type().unwrap(), FileType::Directory);
+    assert!(fs::remove_dir(dirname).is_ok());
 }
